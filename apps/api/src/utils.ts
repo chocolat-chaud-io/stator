45762916e2bf32a5b4d611db083b64379e57ab3f
Gabriel Bottari <gabriel.bottari.defrance@gmail.com>
2020-10-25 19:19:41 -0400

    chore: fix warnings and edit eslint rules

--- apps/api/src/utils.ts
+++ apps/api/src/utils.ts
@@ -1,9 +1,10 @@
 import { ConfigModule, ConfigService } from "@nestjs/config"
+import { ConfigFactory } from "@nestjs/config/dist/interfaces"
 import { TypeOrmModule } from "@nestjs/typeorm"
 
 import { getOrmConfigFn } from "./config/configuration"
 
-export const getRootModuleImports = configuration => [
+export const getRootModuleImports = (configuration: ConfigFactory) => [
   ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
   TypeOrmModule.forRootAsync({
     imports: [ConfigModule],
