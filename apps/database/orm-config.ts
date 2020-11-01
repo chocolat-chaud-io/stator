958951ebf5193c1514fbca7c6017d1aa6c2cb1bf
Yann Thibodeau <y.thibodeau1@gmail.com>
2020-11-01 12:45:58 -0500

    chore: pr fixes

--- apps/database/orm-config.ts
+++ apps/database/orm-config.ts
@@ -7,7 +7,7 @@ const ormConfig: PostgresConnectionOptions = {
   username: process.env.DATABASE_USERNAME || "postgres",
   password: process.env.DATABASE_PASSWORD || "postgres",
   database: process.env.DATABASE_NAME || "stator",
-  synchronize: !!process.env.DATABASE_SYNCHRONIZE || true,
+  synchronize: !!(process.env.DATABASE_SYNCHRONIZE ?? true),
 
   entities: [__dirname + "/../../libs/**/*.entity{.ts,.js}"],
   migrations: [__dirname + "/migrations/*.ts"],
